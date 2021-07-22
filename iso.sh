8abe8216cdee727bb9ab4f718eae606252130731
James Felder <james.m.felder@gmail.com>
2021-07-22 18:51:05 -0400

    Change name to Feline OS.
    Also point Grub to the correct place for modules on my system.

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/FelineOS.kernel isodir/boot/FelineOS.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "myos" {
-	multiboot /boot/myos.kernel
+	multiboot /boot/FelineOS.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o FelineOS.iso isodir -d /usr/lib/grub/i386-pc
