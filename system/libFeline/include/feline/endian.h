b18ca9ee56a5ddb87d49f6d3020f3dbf50760f6d
James McNaughton Felder <james.m.felder@gmail.com>
2023-03-10 19:33:25 -0500

    Templates for dealing with non-native endianness

---
+++ system/libFeline/include/feline/endian.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: MIT */
+/* Copyright (c) 2023 James McNaughton Felder */
+#ifndef _FELINE_ENDIAN_H
+#define _FELINE_ENDIAN_H 1
+
+/*
+ * Some wrappers for writing code that reads structures
+ *   with a specific endianness, but runs on computers
+ *   with different endiannesses.
+ */
+
+#include <feline/reverse_endian.h>
+
+template <typename T>
+class native_endian {
+	public:
+		constexpr inline operator T() const {return value;};
+	private:
+		T value;
+};
+
+template <typename T>
+class nonnative_endian {
+	public:
+		constexpr inline operator T() const {return reverse_endian(value);};
+	private:
+		T value;
+};
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+template <typename T>
+using little_endian = native_endian<T>;
+template <typename T>
+using big_endian = nonnative_endian<T>;
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+template <typename T>
+using big_endian = native_endian<T>;
+template <typename T>
+using little_endian = reverse_endian<T>;
+#else
+#error "Mixed-endian architectures are not supported!"
+#endif
+
+#endif /* _FELINE_ENDIAN_H */
