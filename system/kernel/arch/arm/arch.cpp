d4f09fa090d573402662f3afe1eeb886b11cca57
James McNaughton Felder <james.m.felder@gmail.com>
2023-03-11 17:34:46 -0500

    Remove commented code for handling the screen on ARM

--- system/kernel/arch/arm/arch.cpp
+++ system/kernel/arch/arm/arch.cpp
@@ -16,30 +16,6 @@
 framebuffer fb;
 
 static void screen_init(){
-	/*klogf("Framebuffer at %p.", reinterpret_cast<void*>(mbp.framebuffer_addr));
-	klogf("Frame buffer pitch (in bytes): %u.", mbp.framebuffer_pitch);
-	klogf("It is %ux%u (in pixels).", mbp.framebuffer_width, mbp.framebuffer_height);
-	klogf("With %" PRIu8 " bits per pixel.", mbp.framebuffer_bpp);
-	switch(mbp.framebuffer_type){
-		case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
-			klogf("Framebuffer type: %s.", "indexed");
-			klogf("Palette at %p with %d colors.", reinterpret_cast<void*>(mbp.fb_palette.framebuffer_palette_addr), mbp.fb_palette.framebuffer_palette_num_colors);
-			break;
-		case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
-			klogf("Framebuffer type: %s.", "RGB");
-			klogf("Red field position: %" PRIu8 ", mask size: %" PRIu8, mbp.fb_rgb.framebuffer_red_field_position, mbp.fb_rgb.framebuffer_red_mask_size);
-			klogf("Green field position: %" PRIu8 ", mask size: %" PRIu8, mbp.fb_rgb.framebuffer_green_field_position, mbp.fb_rgb.framebuffer_green_mask_size);
-			klogf("Blue field position: %" PRIu8 ", mask size: %" PRIu8, mbp.fb_rgb.framebuffer_blue_field_position, mbp.fb_rgb.framebuffer_blue_mask_size);
-			break;
-		case MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT:
-			klogf("Framebuffer type: %s.", "EGA text");
-			break;
-	}
-	int fb_init_rval=fb.init(reinterpret_cast<pixel_bgr_t*>(mbp.framebuffer_addr), static_cast<uint16_t>(mbp.framebuffer_width), static_cast<uint16_t>(mbp.framebuffer_height), static_cast<uint16_t>(mbp.framebuffer_pitch), mbp.framebuffer_bpp);
-	if(fb_init_rval!=0){
-		kerrorf("Unable to initialize framebuffer! Failed with error %d.", fb_init_rval);
-		abort();
-	}*/
 }
 
 char grub_cmdline[4096]="";
