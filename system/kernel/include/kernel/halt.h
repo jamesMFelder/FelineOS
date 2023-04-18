18065ed4328ecd1e3e4836beb62fba1f418dbd5f
James McNaughton Felder <james.m.felder@gmail.com>
2023-04-18 14:02:17 -0400

    Add include guard for halt.h

--- system/kernel/include/kernel/halt.h
+++ system/kernel/include/kernel/halt.h
@@ -1,7 +1,11 @@
 /* SPDX-License-Identifier: MIT */
 /* Copyright (c) 2023 James McNaughton Felder */
+#ifndef FELINE_HALT_H
+#define FELINE_HALT_H 1
 
 [[noreturn]] inline void halt() {
 	while(true){}
 	__builtin_unreachable();
 }
+
+#endif /* FELINE_HALT_H */
