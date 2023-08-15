fb76c488e3b19fbfca89a8e2f2065890a920cc1a
James McNaughton Felder <james.m.felder@gmail.com>
2023-08-15 17:02:05 -0400

    Actually use the PMM linked list to track memory
    Now it gets split into correct-sized chunks (4_KiB granularity) when
    being handed out, and merged back together when freed.
    
    I'm using a linked list of arrays of linked-list items. So you have a
    linked list where each item takes up 4_KiB memory, and has just a next
    pointer and a bunch of structs that form a seperate linked list. This
    allows the PMM to only need to self-request memory in 4_KiB chunks,
    while still having many dynamic structs.
    The second linked-list is ordered by address, with no overlaps (but
    possibly gaps). Each entry is either in-use or not. An item not part of
    the chain as the header_in_use bool unset so it can be added safely
    without having to allocate anything new.
    To allocate memory, walk through the inner linked list until you find
    one item that is large enough (there should never be 2 available items
    in a row so you don't need to check for that). Then, set it as used and
    optionally split the end off into a seperate item.
    To deallocate memory, walk through the inner linked list until you find
    the item that points to the memory you want to deallocate. Set it to not
    in use, and merge with the ones before and after if possible
    (remembering to set header_in_use to false for any item that is no
    longer in the chain.
    To allocate a new item, walk through the outer linked list, and at each
    stop iterate through every item at that stop, finishing when you find
    one with header_in_use set to false. If you don't find any, you need to
    allocate a new item in the outer linked list. Then set header_in_use to
    true and use the inner item.
    To deallocate an item, just set header_in_use to false. At the next
    allocation, it will be found and used if needed.

--- system/kernel/kernel/phys_mem.cpp
+++ system/kernel/kernel/phys_mem.cpp
@@ -49,10 +49,17 @@ struct PhysMemHeader {
 	page memory;
 	page size; //number of PHYS_MEM_CHUNK_SIZE chunks (we will lose the ends of areas that don't nicely fit, which is okay)
 	bool in_use;
-	uint8_t canary[3];
+	uint8_t canary[2];
+	bool header_in_use; //is the header controlling memory? (NOT the same as if the memory it points to is in use)
+};
+
+struct PhysMemHeaderList {
+	PhysMemHeaderList *next;
+	PhysMemHeader headers[(PHYS_MEM_CHUNK_SIZE-sizeof(PhysMemHeaderList*))/sizeof(PhysMemHeader)];
 };
 
 static PhysMemHeader *first_header = nullptr;
+static PhysMemHeaderList *first_chunk = nullptr;
 
 int start_phys_mem_manager(
 		struct bootloader_mem_region *unavailable_memory_regions,
@@ -68,7 +75,7 @@ int start_phys_mem_manager(
 
 	/* Is it possible to fit size_needed bytes into location..end_of_available without overlapping something important
 	 * If so, where in the area can we go; if not, nullptr */
-	auto find_space_in_area = [&unavailable_memory_regions, &num_unavailable_memory_regions, &available_memory_regions, &num_available_memory_regions](void const *location, void const *end_of_available, size_t size_needed, auto& recursive_self) -> PhysMemHeader* {
+	auto find_space_in_area = [&unavailable_memory_regions, &num_unavailable_memory_regions, &available_memory_regions, &num_available_memory_regions](void const *location, void const *end_of_available, size_t size_needed, auto& recursive_self) -> PhysMemHeaderList* {
 		/* Align the area to the minimum chunk alignment we can reserve */
 		location=round_up_to_alignment(location, PHYS_MEM_CHUNK_SIZE);
 		enum find_actions {
@@ -146,11 +153,11 @@ int start_phys_mem_manager(
 		}
 		/* Because we reached the end of the loop and did the switch for everything else,
 		 * nothing conflicted with the address, so it works */
-		return reinterpret_cast<PhysMemHeader*>(const_cast<void*>(location));
+		return reinterpret_cast<PhysMemHeaderList*>(const_cast<void*>(location));
 	};
 	/* For each available memory region, check if we can use it */
 	for (size_t i=0; i<num_available_memory_regions; ++i) {
-		first_header=find_space_in_area(
+		first_chunk=find_space_in_area(
 				available_memory_regions[i].addr,
 				reinterpret_cast<void*>(
 					reinterpret_cast<uintptr_t>(available_memory_regions[i].addr)
@@ -158,35 +165,41 @@ int start_phys_mem_manager(
 					),
 				headers_needed_size, find_space_in_area);
 		/* If it's not nullptr, we don't need to check anywhere else because it points to a good place */
-		if (first_header) {
+		if (first_chunk) {
 			break;
 		}
 	}
 
-	if (first_header==nullptr) {
+	if (first_chunk==nullptr) {
 		kcritical("Unable to find space for the memory headers. Aborting!");
 		std::abort();
 	}
 
 	/* Note where we found the memory and stop searching */
 	klog("");
-	klogf("Headers start at %p.", static_cast<void*>(first_header));
-	klogf("Headers end at %p.", static_cast<void*>(first_header+num_headers_needed));
+	klogf("Headers start at %p.", static_cast<void*>(first_chunk));
+	klogf("Headers end at %p.", static_cast<void*>(first_chunk+num_headers_needed));
 	klog("");
 
 	/* Map it, saving it's address to mark it reserved later. */
-	unsigned char *phys_hdr_ptr=reinterpret_cast<unsigned char*>(first_header);
+	unsigned char *phys_hdr_ptr=reinterpret_cast<unsigned char*>(first_chunk);
 	printf("Header chunk is at %p in physical memory...",
-			reinterpret_cast<void*>(first_header));
-	map_results mapping=map_range(first_header,
+			reinterpret_cast<void*>(first_chunk));
+	map_results mapping=map_range(first_chunk,
 			headers_needed_size,
-			reinterpret_cast<void**>(&first_header), 0);
+			reinterpret_cast<void**>(&first_chunk), 0);
 	if(mapping!=map_success){
 		puts("");
 		kcriticalf("Unable to map the physical memory manager (error code %d).", mapping);
 		std::abort();
 	}
-	printf("and %p in virtual memory.\n", reinterpret_cast<void*>(first_header));
+	printf("and %p in virtual memory.\n", reinterpret_cast<void*>(first_chunk));
+	first_chunk->next = nullptr;
+	for (auto &hdr : first_chunk->headers) {
+		/* Clear each field (size=0-0 to choose integer overload instead of void* one for page constructor) */
+		hdr = {.next=nullptr, .prev=nullptr, .memory=nullptr, .size=0-0, .in_use=false, .canary={0, 0}, .header_in_use=false};
+	}
+	first_header = &first_chunk->headers[0];
 
 	/* Copy the available bootloader_mem_regions to the linked list */
 	/* Don't bother keeping the unavailable ones, because we can never free them */
@@ -207,6 +220,7 @@ int start_phys_mem_manager(
 				first_header[cur_header_offset].memory = addr_round_up_to_page(available_memory_regions[cur_avail_region_offset].addr);
 				first_header[cur_header_offset].size = phys_hdr_ptr - addr_round_up_to_page(available_memory_regions[cur_header_offset].addr).getInt();
 				first_header[cur_header_offset].in_use = false;
+				first_header[cur_header_offset].header_in_use = true;
 				++cur_header_offset;
 			}
 			first_header[cur_header_offset].next = nullptr;
@@ -214,6 +228,7 @@ int start_phys_mem_manager(
 			first_header[cur_header_offset].memory = first_header;
 			first_header[cur_header_offset].size = headers_needed_size;
 			first_header[cur_header_offset].in_use = true;
+			first_header[cur_header_offset].header_in_use = true;
 			++cur_header_offset;
 			if (static_cast<unsigned char*>(available_memory_regions[cur_avail_region_offset].addr)+available_memory_regions[cur_avail_region_offset].len != \
 					first_header[cur_header_offset-1].memory+first_header[cur_header_offset-1].size) {
@@ -223,6 +238,7 @@ int start_phys_mem_manager(
 				first_header[cur_header_offset].size = static_cast<unsigned char*>(available_memory_regions[cur_avail_region_offset].addr)+available_memory_regions[cur_avail_region_offset].len-\
 													   (first_header[cur_header_offset-1].memory+first_header[cur_header_offset-1].size).getInt();
 				first_header[cur_header_offset].in_use = false;
+				first_header[cur_header_offset].header_in_use = true;
 				++cur_header_offset;
 			}
 		}
@@ -232,6 +248,7 @@ int start_phys_mem_manager(
 			first_header[cur_header_offset].memory = addr_round_up_to_page(available_memory_regions[cur_avail_region_offset].addr);
 			first_header[cur_header_offset].size = available_memory_regions[cur_avail_region_offset].len-page_offset(available_memory_regions[cur_avail_region_offset].addr);
 			first_header[cur_header_offset].in_use = false;
+			first_header[cur_header_offset].header_in_use = true;
 		}
 		++cur_avail_region_offset;
 		++cur_header_offset;
@@ -243,6 +260,27 @@ int start_phys_mem_manager(
 	return 0;
 }
 
+/* Get the next available header (returns nullptr if none are available) */
+static PhysMemHeader* get_new_header() {
+	PhysMemHeaderList *chunk = first_chunk;
+	/* Go through each chunk of headers */
+	while (chunk != nullptr) {
+		/* And iterate through each header in that chunk */
+		for (auto &header : chunk->headers) {
+			/* Returning the header if we can use it */
+			if (!header.header_in_use) {
+				/* Marking it as used now */
+				header.header_in_use = true;
+				return &header;
+			}
+		}
+		/* None of the headers were available, go to the next chunk */
+		chunk=chunk->next;
+	}
+	/* None of the chunks had an available header (TODO: allocate a new one) */
+	return nullptr;
+}
+
 enum allocation_strategy {
 	first_fit,
 };
@@ -250,10 +288,11 @@ enum allocation_strategy {
 /* Find num unused (contiguous) pages */
 /* modifying_pmm must be locked before calling this! */
 /* returns nullptr if no RAM is available */
-static PhysMemHeader* find_free_pages(size_t len, allocation_strategy strategy[[maybe_unused]]) {
+static PhysMemHeader* find_free_pages(page len, allocation_strategy strategy[[maybe_unused]]) {
 	for (PhysMemHeader *cur_header = first_header; cur_header != nullptr; cur_header = cur_header->next) {
 		/* If it is available and there is enough space */
-		if (!cur_header->in_use && cur_header->size.getInt() >= len) {
+		assert(cur_header->header_in_use);
+		if (!cur_header->in_use && cur_header->size >= len) {
 			return cur_header;
 		}
 	}
@@ -264,6 +303,7 @@ static PhysMemHeader* find_free_pages(size_t len, allocation_strategy strategy[[
 /* Find the header for addr, making sure it is available and has enough space */
 static PhysMemHeader* find_header_for_page(void const * const addr, size_t len) {
 	for (PhysMemHeader *cur_header = first_header; cur_header != nullptr; cur_header = cur_header->next) {
+		assert(cur_header->header_in_use);
 		if (cur_header->memory <= addr && cur_header->memory+cur_header->size >= addr) {
 			/* We found the header, now we just need to check if we can use it */
 			if (cur_header->in_use || cur_header->size.getInt() < len) {
@@ -278,11 +318,48 @@ static PhysMemHeader* find_header_for_page(void const * const addr, size_t len) 
 	return nullptr;
 }
 
-/* Reserve num pages that addr controls */
-/* modifying_pmm must be locked before calling this! */
-static pmm_results internal_claim_mem_area(PhysMemHeader &header, size_t len) {
+/* Make the header point to only len amount of memory if possible */
+/* No garantees, because allocating a new header might be necessary and fail */
+/* However, if it doesn't work, everything is valid, just suboptimal */
+static void split_header_at_len(PhysMemHeader &header, page len) {
+	assert(header.header_in_use);
 	assert(!header.in_use);
-	assert(header.size.getInt() >= len);
+	assert(header.size >= len);
+	if (header.size == len) {
+		return;
+	}
+	/* Get a new header to hold the split */
+	PhysMemHeader *temp = get_new_header();
+	/* We didn't get a new header, so don't split, just return extra memory */
+	/* This might be important if we're reserving more memory for a new chunk */
+	if (temp == nullptr) {
+		kwarn("Unable to get a new header for the PMM! Returning extra memory.");
+		return;
+	}
+	/* Intialize the new header */
+	*temp = PhysMemHeader{
+		.next=header.next,
+		.prev=&header,
+		.memory=header.memory+len,
+		.size=header.size-len,
+		.in_use=false,
+		.canary={0, 0},
+		.header_in_use=true
+	};
+	/* Add it to the chain */
+	if (header.next) {
+		header.next->prev = temp;
+	}
+	header.next = temp;
+	/* And change the size on the returned header */
+	header.size -= temp->size;
+}
+
+/* Reserve the pages that addr controls */
+/* modifying_pmm must be locked before calling this! */
+static pmm_results internal_claim_mem_area(PhysMemHeader &header) {
+	assert(header.header_in_use);
+	assert(!header.in_use);
 	header.in_use = true;
 	return pmm_success;
 }
@@ -294,7 +371,8 @@ pmm_results get_mem_area(void const *const addr, uintptr_t len) {
 	if (!header) {
 		return pmm_nomem;
 	}
-	pmm_results temp = internal_claim_mem_area(*header, len);
+	split_header_at_len(*header, round_up_to_page(len));
+	pmm_results temp = internal_claim_mem_area(*header);
 	modifying_pmm.release_lock();
 	return temp;
 }
@@ -302,13 +380,14 @@ pmm_results get_mem_area(void const *const addr, uintptr_t len) {
 /* Aquire len unused bytes */
 pmm_results get_mem_area(void **addr, uintptr_t len) {
 	modifying_pmm.aquire_lock();
-	auto *header = find_free_pages(len, first_fit);
+	auto *header = find_free_pages(round_up_to_page(len), first_fit);
 	if (header == nullptr) {
 		modifying_pmm.release_lock();
 		return pmm_nomem;
 	}
+	split_header_at_len(*header, round_up_to_page(len));
 	pmm_results result =
-		internal_claim_mem_area(*header, len);
+		internal_claim_mem_area(*header);
 	if (result==pmm_success) {
 		*addr = header->memory;
 	}
@@ -316,10 +395,31 @@ pmm_results get_mem_area(void **addr, uintptr_t len) {
 	return result;
 }
 
+/* Merge the headers next to this one if they are both free */
+void merge_adjacent_headers(PhysMemHeader &header) {
+	if (header.next && !header.next->in_use && header.memory+header.size == header.next->memory) {
+		header.next->header_in_use = false;
+		header.size += header.next->size;
+		if (header.next->next) {
+			header.next->next->prev = &header;
+		}
+		header.next = header.next->next;
+	}
+	if (header.prev && !header.prev->in_use && header.prev->memory+header.prev->size == header.memory) {
+		header.header_in_use = false;
+		header.prev->size += header.size;
+		header.prev->next = header.next;
+		if (header.next) {
+			header.next->prev = header.prev;
+		}
+	}
+}
+
 /* Free len bytes from addr */
 pmm_results free_mem_area(void const *const addr, uintptr_t len) {
 	modifying_pmm.aquire_lock();
 	for (PhysMemHeader *cur_header = first_header; cur_header != nullptr; cur_header = cur_header->next) {
+		assert(cur_header->header_in_use);
 		if (cur_header->memory <= addr && cur_header->memory+cur_header->size >= addr) {
 			auto end = static_cast<unsigned char const *>(addr)+len;
 			if (cur_header->memory+cur_header->size < end) {
@@ -332,6 +432,7 @@ pmm_results free_mem_area(void const *const addr, uintptr_t len) {
 				return pmm_invalid;
 			}
 			cur_header->in_use = false;
+			merge_adjacent_headers(*cur_header);
 			/* TODO: merge with free regions */
 			modifying_pmm.release_lock();
 			return pmm_success;
