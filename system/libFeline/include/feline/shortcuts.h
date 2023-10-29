a9ae0817e85e13c23ea6884800167b0896e2abc9
James McNaughton Felder <james.m.felder@gmail.com>
2023-10-29 15:02:24 -0400

    Add begin() and end() wrapper functions
    These wrap over arrays (with a template for the size) and objects that
    have .data() and .size() member functions.
    
    C++ has some magic with its for-loops that allow iterating over
    containers that have .begin() and .end() member functions, but we don't
    have any of those in the kernel, so I'm not worried about unifying those
    as well.

---
+++ system/libFeline/include/feline/shortcuts.h
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: MIT */
+/* Copyright (c) 2023 James McNaughton Felder */
+#ifndef _FELINE_SHORTCUTS_H
+#define _FELINE_SHORTCUTS_H 1
+
+#include <cstddef>
+#include <feline/cpp_only.h>
+
+template <typename T>
+concept container = requires(T c) {
+	{c.size()};
+	{c.data()};
+	{c[0]};
+	typename T::value_type;
+	typename T::iterator;
+};
+
+template <typename T>
+concept needs_begin_end = requires(T container) {
+	!container.begin() && !container.end();
+};
+
+template <container T>
+requires requires {needs_begin_end<T>;}
+T::iterator begin(T c) {
+	return c.data();
+}
+
+template <container T>
+requires requires {needs_begin_end<T>;}
+T::iterator end(T c) {
+	return c.data() + c.size();
+}
+
+template <typename T, size_t N>
+T* begin(T (&array)[N]) {
+	return array;
+}
+
+template <typename T, size_t N>
+T* end(T (&array)[N]) {
+	return array + N;
+}
+
+#endif /* _FELINE_SHORTCUTS_H */
