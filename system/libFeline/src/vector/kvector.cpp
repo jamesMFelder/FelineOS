b679adc856b0acc74ca854727352b324d9ba5369
James McNaughton Felder <james.m.felder@gmail.com>
2024-01-29 15:55:25 -0500

    Allow building libFeline by itself

--- system/libFeline/src/vector/kvector.cpp
+++ system/libFeline/src/vector/kvector.cpp
@@ -1,9 +1,17 @@
 #include <feline/kvector.h>
+#ifndef LIBFELINE_ONLY
 #include <kernel/log.h>
+#else
+#include <iostream>
+#endif
 
 void check_index(size_t index, size_t size) {
 	if (index >= size) {
+#ifndef LIBFELINE_ONLY
 		kError() << "Index " << dec(index) << " >= size " << dec(size);
+#else
+		std::cerr << "Index " << index << " >= size " << size << std::endl;
+#endif
 		std::abort();
 	}
 }
