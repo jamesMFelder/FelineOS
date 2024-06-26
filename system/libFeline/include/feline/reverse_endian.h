8568eec18a88471c348c2d8be75bca8e3970693c
James McNaughton Felder <james.m.felder@gmail.com>
2024-06-25 20:47:50 -0400

    Allow reverse_endian to take const values

--- system/libFeline/include/feline/reverse_endian.h
+++ system/libFeline/include/feline/reverse_endian.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: MIT */
 /* Copyright (c) 2023 James McNaughton Felder */
+#include <type_traits>
 #ifndef _FELINE_REVERSE_ENDIAN_H
 #define _FELINE_REVERSE_ENDIAN_H 1
 
@@ -8,14 +9,14 @@
 
 template <typename T> constexpr T reverse_endian(T value);
 
-inline uint16_t reverse_endian(uint16_t value) {
+constexpr inline uint16_t reverse_endian(const uint16_t value) {
 	uint16_t reversed = 0;
 	reversed |= (value & 0xff00) >> 8;
 	reversed |= (value & 0x00ff) << 8;
 	return reversed;
 }
 
-inline uint32_t reverse_endian(uint32_t value) {
+constexpr inline uint32_t reverse_endian(const uint32_t value) {
 	uint32_t reversed = 0;
 	reversed |= (value & 0xff000000) >> 24;
 	reversed |= (value & 0x00ff0000) >> 8;
@@ -24,7 +25,7 @@ inline uint32_t reverse_endian(uint32_t value) {
 	return reversed;
 }
 
-inline uint64_t reverse_endian(uint64_t value) {
+constexpr inline uint64_t reverse_endian(const uint64_t value) {
 	uint64_t reversed = 0;
 	reversed |= (value & 0xff00000000000000) >> 56;
 	reversed |= (value & 0x00ff000000000000) >> 40;
@@ -39,7 +40,7 @@ inline uint64_t reverse_endian(uint64_t value) {
 
 template <typename T>
 concept EndianReverseable = requires(T a) {
-	{ reverse_endian(a) } -> std::same_as<T>;
+	{ reverse_endian(a) } -> std::same_as<std::remove_const_t<T>>;
 };
 
 #endif /* _FELINE_REVERSE_ENDIAN_H */
