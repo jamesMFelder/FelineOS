5a12a305520e14810638b7909450956daed40c5b
James McNaughton Felder <james.m.felder@gmail.com>
2024-07-04 09:04:16 -0700

    Add tests for KVector

---
+++ system/libFeline/tests/kvector.cpp
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: MIT */
+/* Copyright (c) 2024 James McNaughton Felder */
+
+#include <feline/tests.h>
+
+int main() {
+	initialize_loggers();
+	KVector<uint8_t, KGeneralAllocator<uint8_t>> vec;
+	REQUIRE_EQ(vec.capacity(), 0uz);
+	REQUIRE_EQ(vec.size(), 0uz);
+	vec.reserve(8);
+	REQUIRE_EQ(vec.capacity(), 8uz);
+	REQUIRE_EQ(vec.size(), 0uz);
+
+	for (size_t i = 0; i < 8; ++i) {
+		vec.push_back(i);
+	}
+
+	REQUIRE_EQ(vec.capacity(), 8uz);
+	REQUIRE_EQ(vec.size(), 8uz);
+
+	for (auto &elem : vec) {
+		REQUIRE_EQ(elem, std::distance(begin(vec), &elem));
+	}
+
+	vec.clear();
+
+	REQUIRE_EQ(vec.capacity(), 8uz);
+	REQUIRE_EQ(vec.size(), 0uz);
+}
