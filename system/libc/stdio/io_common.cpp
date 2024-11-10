2fe9d89e22491c3bc096c6af8ffb536e69fd04c2
James McNaughton Felder <james.m.felder@gmail.com>
2024-11-10 16:44:02 -0500

    Don't try to use the VGA terminal on ARM

--- system/libc/stdio/io_common.cpp
+++ system/libc/stdio/io_common.cpp
@@ -4,21 +4,27 @@
 
 #if defined(__is_libk)
 
+#ifdef __i386__
 extern vga_text_term term;
+
+static int term_putchar(char const c) { return term.putchar(c); }
+#else  // __i386__
+int term_putchar(char const) { return 0; }
+#endif // __i386__ (else)
 
 int __internal_putchar(char const c) {
 	// TODO: check if in text mode first
 	if (c == '\n')
 		__internal_putchar('\r');
 	put_serial(c);
-	term.putchar(c);
+	term_putchar(c);
 	return 0;
 }
 
 int __internal_writeStr(char const *const s) {
 	writestr_serial(s);
 	for (const char *c = s; *c != '\0'; c++) {
-		term.putchar(*c);
+		term_putchar(*c);
 	}
 	return 0;
 }
