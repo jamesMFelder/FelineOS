637fa08115a890be51464d632276f159f625484e
James McNaughton Felder <james.m.felder@gmail.com>
2024-11-17 16:19:47 -0500

    Print abort() message with libFeline's loggers
    Replaces the kcriticalf, which also means it can be potentially
    unchanged when porting to userspace.

--- system/libc/stdlib/abort.cpp
+++ system/libc/stdlib/abort.cpp
@@ -1,20 +1,21 @@
 /* SPDX-License-Identifier: MIT */
 /* Copyright (c) 2023 James McNaughton Felder */
-#include <cstdlib>
+
+#include <bits/c_compat.h>
+#include <feline/logger.h>
 
 #if defined(__is_libk)
 #include <kernel/backtrace.h>
-#include <kernel/log.h>
 #endif /* __is_libk */
 
-__attribute__((__noreturn__)) void abort(void) {
+C_LINKAGE __attribute__((__noreturn__)) void abort(void) {
+	kCriticalNoAlloc() << "std::abort()";
 #if defined(__is_libk)
 	/* TODO: Add proper kernel panic. */
-	kerror("std::abort()");
 	backtrace();
-#else  /* __is_libk */
+#else /* __is_libk */
 	/* TODO: Abnormally terminate the process as if by SIGABRT. */
-	printf("std::abort()\n");
+#error "Userspace std::abort not supported yet!"
 #endif /* __is_libk (else) */
 	while (true) {
 	}
