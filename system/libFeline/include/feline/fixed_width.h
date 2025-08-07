e5ad58765f164c4c3ae2fc99bad11c9733e6e11f
James McNaughton Felder <james.m.felder@gmail.com>
2025-08-07 06:23:18 -0400

    Remove __attribute__((force_inline)) for libFeline

--- system/libFeline/include/feline/fixed_width.h
+++ system/libFeline/include/feline/fixed_width.h
@@ -7,60 +7,60 @@
 
 #include <cstdint>
 
-constexpr inline uint8_t __attribute__((always_inline))
+constexpr inline uint8_t
 operator"" _uint8_t(unsigned long long num) {
 	return static_cast<uint8_t>(num);
 }
-constexpr inline int8_t __attribute((always_inline))
+constexpr inline int8_t
 operator"" _int8_t(unsigned long long num) noexcept {
 	return static_cast<int8_t>(num);
 }
 
-constexpr inline uint16_t __attribute((always_inline))
+constexpr inline uint16_t
 operator"" _uint16_t(unsigned long long num) noexcept {
 	return static_cast<uint16_t>(num);
 }
-constexpr inline int16_t __attribute((always_inline))
+constexpr inline int16_t
 operator"" _int16_t(unsigned long long num) noexcept {
 	return static_cast<int16_t>(num);
 }
 
-constexpr inline uint32_t __attribute((always_inline))
+constexpr inline uint32_t
 operator"" _uint32_t(unsigned long long num) noexcept {
 	return static_cast<uint32_t>(num);
 }
-constexpr inline int32_t __attribute((always_inline))
+constexpr inline int32_t
 operator"" _int32_t(unsigned long long num) noexcept {
 	return static_cast<int32_t>(num);
 }
 
-constexpr inline uint64_t __attribute((always_inline))
+constexpr inline uint64_t
 operator"" _uint64_t(unsigned long long num) noexcept {
 	return static_cast<uint64_t>(num);
 }
-constexpr inline int64_t __attribute((always_inline))
+constexpr inline int64_t
 operator"" _int64_t(unsigned long long num) noexcept {
 	return static_cast<int64_t>(num);
 }
 
-constexpr inline intptr_t __attribute((always_inline))
+constexpr inline intptr_t
 operator"" _intptr_t(unsigned long long num) noexcept {
 	return static_cast<intptr_t>(num);
 }
-constexpr inline uintptr_t __attribute((always_inline))
+constexpr inline uintptr_t
 operator"" _uintptr_t(unsigned long long num) noexcept {
 	return static_cast<uintptr_t>(num);
 }
 
-constexpr inline unsigned long long __attribute((always_inline))
+constexpr inline unsigned long long
 operator"" _KiB(unsigned long long KiB) noexcept {
 	return KiB * 1024ull;
 }
-constexpr inline unsigned long long __attribute((always_inline))
+constexpr inline unsigned long long
 operator"" _MiB(unsigned long long MiB) noexcept {
 	return MiB * 1024ull * 1024ull;
 }
-constexpr inline unsigned long long __attribute((always_inline))
+constexpr inline unsigned long long
 operator"" _GiB(unsigned long long GiB) noexcept {
 	return GiB * 1024ull * 1024ull * 1024ull;
 }
