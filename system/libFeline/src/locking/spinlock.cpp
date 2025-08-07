344ba6b29f0f76be579f6f14e108b31663e02fe5
James McNaughton Felder <james.m.felder@gmail.com>
2025-08-07 06:14:04 -0400

    Use __nop() instead of asm("nop") on MSVC

--- system/libFeline/src/locking/spinlock.cpp
+++ system/libFeline/src/locking/spinlock.cpp
@@ -1,5 +1,15 @@
 #include <feline/spinlock.h>
 
+// TODO: find a better home for this
+// https://stackoverflow.com/a/54920142
+#ifdef _MSC_VER
+#include <intrin.h>
+#define NOP() __nop()       // _emit 0x90
+#else
+// assume __GNUC__ inline asm
+#define NOP() asm("nop")    // implicitly volatile
+#endif
+
 /* Wait to get the lock */
 void Spinlock::acquire_lock() {
 	bool temp = false;
@@ -8,7 +18,7 @@ void Spinlock::acquire_lock() {
 	while (!lock.compare_exchange_weak(temp, true, std::memory_order_seq_cst)) {
 		temp = false;
 		/* If it wasn't 0, relax the CPU so hyper-threading is more efficient */
-		asm("nop");
+		NOP();
 	}
 	/* Once it was 0 (released by someone else) */
 	/*	We already set it to 1 */
